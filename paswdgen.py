d54f44f7022f674445d8f12af4bcc6190eedc6d1
imindecent <54386010+imindecent@users.noreply.github.com>
2020-12-08 22:46:19 -1000

    Add files via upload

---
+++ paswdgen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import random
+def x(): a = input(" <password\nlength(#)> "); [g() for j in range(int(a))] if int(a)>=15 else print("add more length to pass");x()
+def g(): s = "1qazZAQ!2wsxXSW@3edcCDE#4rfvVFR$5tgbBGT%6yhnNHY&ujmMJU&8ikKI*9olp";b = random.randint(1,64);e = b + 1;print(s[b:e], end="")
+x()
+#this code does stuff lol
