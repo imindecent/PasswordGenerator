b3c4d553738c41db6ce994e614f088b36c1a61ed
imindecent <54386010+imindecent@users.noreply.github.com>
2021-02-03 15:05:30 -1000

    Update paswdgen.py

--- paswdgen.py
+++ paswdgen.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import random
-def x(): a = input(" <password\nlength(#)> "); [g() for j in range(int(a))] if int(a)>=15 else print("add more length to pass");x()
-def g(): s = "1qazZAQ!2wsxXSW@3edcCDE#4rfvVFR$5tgbBGT%6yhnNHY&ujmMJU&8ikKI*9olp";b = random.randint(1,64);e = b + 1;print(s[b:e], end="")
-x()
-#this code does stuff lol
+def g(): print(''.join(random.choice('1qazZAQ!2wsxXSW@3edcCDE#4rfvVFR$5tgbBGT%6yhnNHY&ujmMJU&8ikKI*9olp')), end="")
+a = input("len(#)> ");[g() for i in range(int(a))];print("\n^passwd")
