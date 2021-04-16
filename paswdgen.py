88e47ba69b9475244047331e97f2fc06129eba80
imindecent <54386010+imindecent@users.noreply.github.com>
2021-04-16 15:01:37 -0400

    Update paswdgen.py

--- paswdgen.py
+++ paswdgen.py
@@ -1,4 +1,1 @@
-#!/usr/bin/python3
-import random
-def g(): print(''.join(random.choice('1qazZAQ!2wsxXSW@3edcCDE#4rfvVFR$5tgbBGT%6yhnNHY&ujmMJU&8ikKI*9olp')), end="")
-a = input("len(#)> ");[g() for i in range(int(a))];print("\n^passwd")
+python3 -c "import random as r;a=$1;[print(''.join(r.choice('1qazZAQ!2wsxXSW@3edcCDE#4rfvVFR$5tgbBGT%6yhnNHY&ujmMJU&8ikKI*9olp')),end='') for i in range(int(a))];print('')"
